2c98babb15c9219a447be670f100c81c1ca985d9
wensespl <wpenadillol@uni.pe>
2022-01-17 17:54:14 -0500

    avance 3

--- src/components/Page1.tsx
+++ src/components/Page1.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import NavigationEstudents from "./NavigationEstudents";
 
 export default function Page1() {
   return (
-    <Container className="mt-3 p-3 border border-warning rounded-3">
-      <p className="fw-bold fs-5">Mallas curriculares</p>
-      <p>Malla General</p>
-      <p>Malla Análisi Numérico y Ecuaciones Diferenciales</p>
-      <p>Malla Optimización y Control</p>
-      <p>Malla Estadistica y Procesos Estocasticos</p>
-      <p className="fw-bold fs-5">Plan de estudios</p>
-      <p>Plan de Estudios Vigente</p>
-    </Container>
+    <>
+      <NavigationEstudents />
+      <Container className="mt-3 p-3 border border-warning rounded-3">
+        <p className="fw-bold fs-5">Mallas curriculares</p>
+        <p>Malla General</p>
+        <p>Malla Análisi Numérico y Ecuaciones Diferenciales</p>
+        <p>Malla Optimización y Control</p>
+        <p>Malla Estadistica y Procesos Estocasticos</p>
+        <p className="fw-bold fs-5">Plan de estudios</p>
+        <p>Plan de Estudios Vigente</p>
+      </Container>
+    </>
   );
 }
