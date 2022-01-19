63e65e9ddab2341bfb80f98d2d928aef5f65b663
wensespl <wpenadillol@uni.pe>
2022-01-19 17:43:28 -0500

    Update NavigationEstudents.tsx

--- src/components/NavigationEstudents.tsx
+++ src/components/NavigationEstudents.tsx
@@ -4,22 +4,25 @@ import routes from "../helpers/routes";
 
 export default function NavigationEstudents() {
   return (
-    <Navbar variant="dark" bg="dark">
-      <Container>
-        <Navbar.Collapse>
-          <Nav className="mx-auto px-3">
-            <Nav.Link as={NavLink} to={routes.page1}>
-              Planes y mallas
-            </Nav.Link>
-            <Nav.Link as={NavLink} to={routes.page2}>
-              Perfil de egreso
-            </Nav.Link>
-            <Nav.Link as={NavLink} to={routes.page3}>
-              Enlaces de interés
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Container>
+      <Container className="pt-5" />
+      <Navbar style={{ backgroundColor: "#1D3557" }} variant="dark">
+        <Container>
+          <Navbar.Collapse>
+            <Nav className="mx-auto px-3">
+              <Nav.Link as={NavLink} to={routes.page1}>
+                Planes y mallas
+              </Nav.Link>
+              <Nav.Link as={NavLink} to={routes.page2}>
+                Perfil de egreso
+              </Nav.Link>
+              <Nav.Link as={NavLink} to={routes.page3}>
+                Enlaces de interés
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Container>
   );
 }
