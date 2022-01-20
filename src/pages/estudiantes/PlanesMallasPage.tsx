3271534d9ea67ece323abed69aa88c35df0c8c7b
wensespl <wpenadillol@uni.pe>
2022-01-20 12:59:52 -0500

    minor changes

--- src/pages/estudiantes/PlanesMallasPage.tsx
+++ src/pages/estudiantes/PlanesMallasPage.tsx
@@ -2,20 +2,20 @@ import { Container } from "react-bootstrap";
 
 export default function PlanesMallasPage() {
   return (
-    <>
-      <Container className="text-start p-3">
-        <p className="fw-bold fs-5">Mallas curriculares</p>
-        <Container>
-          <p>Malla General</p>
-          <p>Malla Análisi Numérico y Ecuaciones Diferenciales</p>
-          <p>Malla Optimización y Control</p>
-          <p>Malla Estadistica y Procesos Estocasticos</p>
-        </Container>
-        <p className="fw-bold fs-5">Plan de estudios</p>
-        <Container>
-          <p>Plan de Estudios Vigente</p>
-        </Container>
+    <Container className="text-start p-3">
+      <p className="fw-bold fs-5">Mallas curriculares</p>
+      <Container>
+        <a className="a-link" href="./PLAN-MATEMATICA-2018.pdf">
+          Malla General
+        </a>
+        <p>Malla Análisi Numérico y Ecuaciones Diferenciales</p>
+        <p>Malla Optimización y Control</p>
+        <p>Malla Estadistica y Procesos Estocasticos</p>
       </Container>
-    </>
+      <p className="fw-bold fs-5">Plan de estudios</p>
+      <Container>
+        <p>Plan de Estudios Vigente</p>
+      </Container>
+    </Container>
   );
 }
