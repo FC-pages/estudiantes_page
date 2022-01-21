575c4d5a093ff3c514c692cf691f3b583c58107c
wensespl <wpenadillol@uni.pe>
2022-01-21 16:22:41 -0500

    Update PerfilEgresoPage.tsx

--- src/pages/estudiantes/PerfilEgresoPage.tsx
+++ src/pages/estudiantes/PerfilEgresoPage.tsx
@@ -14,6 +14,51 @@ export default function PerfilEgresoPage() {
           se complementa con una moderna y avanzada preparación
           numérico-computacional.
         </p>
+        <p>Esta formación capacita a nuestros egresados para:</p>
+        <Container className="ps-3">
+          <p>
+            Dar solución a problemas provenientes de los ámbitos tecnológicos o
+            científicos, a través de:
+          </p>
+          <Container className="ps-3">
+            <p>
+              la formulación, diseño, análisis y validación de modelos
+              matemáticos
+            </p>
+            <p>la utilización de métodos numéricos y estadísticos</p>
+            <p>el desarrollo e implementación de algoritmos</p>
+            <p>
+              el manejo eficiente de herramientas y técnicas computacionales
+            </p>
+            <p>el control de modelos predictivos.</p>
+          </Container>
+          <p>
+            Participar en proyectos de investigación que involucren matemática o
+            ingeniería.
+          </p>
+          <p>
+            Dirigir e interactuar con equipos multidisciplinarios para dar
+            solución a problemas complejos de la Ingeniería de manera segura,
+            procurando un mínimo impacto ambiental.
+          </p>
+          <p>
+            Planificar, evaluar, asesorar e investigar en procesos productivos y
+            de servicios.
+          </p>
+          <p>
+            Transmitir el conocimiento sobre Ingeniería Matemática a estudiantes
+            de educación superior y a profesionales en todo ámbito.
+          </p>
+          <p>
+            Enfrentar adecuadamente las exigencias y responsabilidades propias
+            del liderazgo en la empresa moderna, demostrando un carácter
+            innovador y emprendedor.
+          </p>
+          <p>
+            Supervisar, administrar y controlar recursos humanos, informáticos y
+            económicos.
+          </p>
+        </Container>
       </Container>
     </>
   );
