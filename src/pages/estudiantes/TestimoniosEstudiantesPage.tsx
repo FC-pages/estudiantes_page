fc259c7386c7572217eb8e96f9084ef6ab1a3a17
wensespl <wpenadillol@uni.pe>
2022-01-24 15:45:11 -0500

    avance

--- src/pages/estudiantes/TestimoniosEstudiantesPage.tsx
+++ src/pages/estudiantes/TestimoniosEstudiantesPage.tsx
@@ -1,23 +1,51 @@
-import { Container, Card } from "react-bootstrap";
+import { Container, Card, Row, Col } from "react-bootstrap";
 import ReactPlayer from "react-player";
 
 export default function TestimoniosEstuduantesPage() {
   return (
-    <Container className="p-3">
-      <Card style={{ width: "40%" }}>
-        <ReactPlayer
-          controls
-          width="100%"
-          url="https://www.youtube.com/watch?v=yDyMSliKsxI"
-        />
-        <Card.Body>
-          <Card.Title>Nombre Estudiante</Card.Title>
-          <Card.Text>
-            Descripcion del estudiante Descripcion del estudiante Descripcion
-            del estudiante
-          </Card.Text>
-        </Card.Body>
-      </Card>
+    <Container className="text-start p-3">
+      <Row>
+        <Col>
+          <Card style={{ width: "100%" }}>
+            <ReactPlayer
+              controls
+              width="100%"
+              url="https://www.youtube.com/watch?v=yDyMSliKsxI"
+            />
+            <Card.Body>
+              <Card.Title>Nombre Estudiante 1</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                dd/mm/aaaa
+              </Card.Subtitle>
+              <Card.Text>
+                Descripción del estudiante Lorem ipsum dolor sit amet,
+                consectetur adipisicing elit. Perferendis sint illum iusto
+                nostrum cumque qui voluptas
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card style={{ width: "100%" }}>
+            <ReactPlayer
+              controls
+              width="100%"
+              url="https://www.youtube.com/watch?v=yDyMSliKsxI"
+            />
+            <Card.Body>
+              <Card.Title>Nombre Estudiante 2</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                dd/mm/aaaa
+              </Card.Subtitle>
+              <Card.Text>
+                Descripción del estudiante Lorem ipsum dolor sit amet,
+                consectetur adipisicing elit. Perferendis sint illum iusto
+                nostrum cumque qui voluptas
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   );
 }
