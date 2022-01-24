7ff94418435ad5a6d4bd6d26530ad054c0674370
wensespl <wpenadillol@uni.pe>
2022-01-24 18:53:20 -0500

    Update EnlacesInteresPage.tsx

--- src/pages/EnlacesInteres/EnlacesInteresPage.tsx
+++ src/pages/EnlacesInteres/EnlacesInteresPage.tsx
@@ -20,7 +20,8 @@ export default function EnlacesInteresPage() {
       <a href="https://images.math.cnrs.fr/">https://images.math.cnrs.fr/</a>
       <br />
       <a href="https://zbmath.org/">https://zbmath.org/</a>
-      <p className="fw-bold fs-5">Enlaces de instituciones</p>
+
+      <p className="fw-bold fs-5 mt-2">Enlaces de instituciones</p>
       <a href="http://www.imca.edu.pe/">http://www.imca.edu.pe/</a>
       <br />
       <a href="https://impa.br/">https://impa.br/</a>
@@ -41,6 +42,59 @@ export default function EnlacesInteresPage() {
       <br />
       <a href="https://www.ictp.it/research/math.aspx">
         https://www.ictp.it/research/math.aspx
+      </a>
+
+      <p className="fw-bold fs-5 mt-2">Enlaces para cursos</p>
+      <a href="https://la.mathworks.com/academia/students.html">
+        https://la.mathworks.com/academia/students.html
+      </a>
+      <br />
+      <a href="https://pythones.net/">https://pythones.net/</a>
+      <br />
+      <a href="https://www.geogebra.org/classic?lang=es">
+        https://www.geogebra.org/classic?lang=es
+      </a>
+
+      <p className="fw-bold fs-5 mt-2">Enlaces a competiciones</p>
+      <a href="https://imaginecup.microsoft.com/es-es/Compete/">
+        https://imaginecup.microsoft.com/es-es/Compete/
+      </a>
+      <br />
+      <a href="https://www.cuandopasa.com/m/index.php?v=v98102c">
+        https://www.cuandopasa.com/m/index.php?v=v98102c
+      </a>
+
+      <p className="fw-bold fs-5 mt-2">Otros</p>
+      <a href="https://www.uni.edu.pe/index.php/acerca/225-reglamentos-directivas-manuales-y-otros-documentos-institucionales">
+        https://www.uni.edu.pe/index.php/acerca/225-reglamentos-directivas-manuales-y-otros-documentos-institucionales
+      </a>
+      <br />
+      <a href="https://www.sunedu.gob.pe/registro-nacional-de-grados-y-titulos/">
+        https://www.sunedu.gob.pe/registro-nacional-de-grados-y-titulos/
+      </a>
+      <br />
+      <a href="https://www.gob.pe/prociencia">https://www.gob.pe/prociencia</a>
+      <br />
+      <a href="https://www.conicyt.cl/becasconicyt/">
+        https://www.conicyt.cl/becasconicyt/
+      </a>
+      <br />
+      <a href="http://pec3.org">http://pec3.org</a>
+      <br />
+      <a href="https://www.ieee.org/">https://www.ieee.org/</a>
+      <br />
+      <a href="https://www.routeyou.com/es-us/location/view/51387132/sociedad-de-matematicas-aplicadas-e-industriales">
+        https://www.routeyou.com/es-us/location/view/51387132/sociedad-de-matematicas-aplicadas-e-industriales
+      </a>
+      <br />
+      <a href="https://iciam.org/history-iciam">
+        https://iciam.org/history-iciam
+      </a>
+      <br />
+      <a href="https://www.mathunion.org/">https://www.mathunion.org/</a>
+      <br />
+      <a href="https://www.rsme.es/2022/01/ultimo-boletin-2/">
+        https://www.rsme.es/2022/01/ultimo-boletin-2/
       </a>
     </Container>
   );
