056920fb2628935d94ca3a77dad92b01b66c11c3
wensespl <wpenadillol@uni.pe>
2022-01-26 15:49:17 -0500

    avance enlaces de interes

--- src/pages/EnlacesInteres/EnlacesInteresPage.tsx
+++ src/pages/EnlacesInteres/EnlacesInteresPage.tsx
@@ -21,17 +21,25 @@ export default function EnlacesInteresPage() {
         <Tab eventKey="Revistas sobre ciencias" title="Revistas sobre ciencias">
           <Container className="p-3">
             <p className="fs-4 fw-bold">
-              1. <a href="https://www.larecherche.fr/">La Recherche</a>
+              1.{" "}
+              <a className="a-link-title" href="https://www.larecherche.fr/">
+                La Recherche
+              </a>
             </p>
             <p>
-              La Recherche is a monthly French language popular science magazine
-              covering recent scientific news. It is published by the Société
-              d'éditions scientifiques, a subsidiary of Financière Tallandier.
-              Tallandier is owned by Artémis, an investment company of
-              François-Henri Pinault. The headquarters is in Paris.
+              La Recherche es una revista mensual de divulgación científica en
+              francés cubriendo noticias científicas recientes. Es publicado por
+              la Société d'éditions scientifiques, filial de Financière
+              Tallandier. Tallandier es propiedad de Artémis, una sociedad de
+              inversión de François-Henri Pinault. La sede se encuentra en
+              París.
             </p>
             <img
-              style={{ backgroundColor: "#126f91", height: "70px" }}
+              style={{
+                backgroundColor: "#126f91",
+                height: "70px",
+                padding: "0 10px",
+              }}
               alt="La Recherche"
               src="./logo_lr.png"
             />
@@ -40,15 +48,21 @@ export default function EnlacesInteresPage() {
 
             <p className="mt-3 fs-4 fw-bold">
               2.{" "}
-              <a href="https://www.sciencesetavenir.fr/">Sciences et avenir</a>
+              <a
+                className="a-link-title"
+                href="https://www.sciencesetavenir.fr/"
+              >
+                Sciences et avenir
+              </a>
             </p>
             <p>
-              Toute l'actualité scientifique est sur sciencesetavenir.fr :
-              sciences naturelles, sciences humaines, environnement,
-              archéologie, exploration spatiale...
+              Sciences et Avenir es una revista mensual francesa de divulgación
+              científica, propiedad de Claude Perdriel. Su distribución en 2019
+              fue de 231.000 ejemplares. El equipo editorial también publica
+              alrededor de 15 artículos por día en su sitio web.
             </p>
             <img
-              style={{ backgroundColor: "#126f91", height: "70px" }}
+              style={{ height: "70px" }}
               alt="Sciences et avenir"
               src="./logo_sa.svg"
             />
@@ -57,34 +71,62 @@ export default function EnlacesInteresPage() {
 
             <p className="mt-3 fs-4 fw-bold">
               3.{" "}
-              <a href="https://www.sciencesetavenir.fr/">
-                https://www.sciencesetavenir.fr/
+              <a className="a-link-title" href="https://www.pourlascience.fr/">
+                Pour la science
               </a>
             </p>
+            <p>
+              Pour la Science es la revista de referencia para las noticias
+              científicas, con artículos en profundidad de los mejores
+              investigadores en todos los campos.
+            </p>
+            <img
+              style={{ height: "70px" }}
+              alt="Pour la science"
+              src="./logo_pls.png"
+            />
 
             <br />
 
             <p className="mt-3 fs-4 fw-bold">
               4.{" "}
-              <a href="https://www.pourlascience.fr/">
-                https://www.pourlascience.fr/
+              <a className="a-link-title" href="https://images.math.cnrs.fr/">
+                Images des Mathématiques
               </a>
             </p>
+            <p>
+              Images des mathématiques es un sitio web francés publicado por el
+              CNRS y dedicado a la popularización de las matemáticas. Su
+              objetivo es mostrar la diversidad de las matemáticas de una manera
+              atractiva. Está subtitulada la búsqueda matemática en palabras e
+              imágenes.
+            </p>
+            <img
+              style={{ height: "80px" }}
+              alt="Images des Mathématiques"
+              src="./logo-idm-fr.png"
+            />
 
             <br />
 
             <p className="mt-3 fs-4 fw-bold">
               5.{" "}
-              <a href="https://images.math.cnrs.fr/">
-                https://images.math.cnrs.fr/
+              <a className="a-link-title" href="https://zbmath.org/">
+                zbMATH Open
               </a>
             </p>
-
-            <br />
-
-            <p className="mt-3 fs-4 fw-bold">
-              6. <a href="https://zbmath.org/">https://zbmath.org/</a>
+            <p>
+              zbMATH, anteriormente Zentralblatt MATH, es un importante servicio
+              de revisión que proporciona revisiones y resúmenes de artículos en
+              matemáticas puras y aplicadas, producido por la oficina de Berlín
+              de FIZ Karlsruhe – Leibniz Institute for Information
+              Infrastructure GmbH.
             </p>
+            <img
+              style={{ height: "65px" }}
+              alt="Pour la science"
+              src="./zbMATH.gif"
+            />
           </Container>
         </Tab>
         <Tab
