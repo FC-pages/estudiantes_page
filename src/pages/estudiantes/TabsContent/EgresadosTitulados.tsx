72057a1966bc5bf4e30fd557ef326ca1db54cdbb
wensespl <wpenadillol@uni.pe>
2022-01-27 17:48:39 -0500

    cambio

--- src/pages/estudiantes/TabsContent/EgresadosTitulados.tsx
+++ src/pages/estudiantes/TabsContent/EgresadosTitulados.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { Container, Tab, Nav } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import Titulados from "../../Egresados/Titulados";
-import Egresados from "../../Egresados/Egresados";
 
 export default function EgresadosTitulados() {
   return (
     <Container>
-      <Titulados/> 
+      <Titulados />
     </Container>
   );
 }
