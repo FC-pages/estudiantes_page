87b4510ff2d209904369c4d43d4b0a78c070bda3
abrahanrm <araymundom@uni.pe>
2022-02-05 00:27:02 -0500

    centrando-grid-miembros-gInvestigacion

--- src/pages/GruposInvestigacion/Optimizacion/Miembros.tsx
+++ src/pages/GruposInvestigacion/Optimizacion/Miembros.tsx
@@ -20,195 +20,219 @@ const TabTwo: FC<{}> = () => {
         <h3 className="t-stroke-shadow">Línea de Optimización</h3>
       </div>
       <hr></hr>
-      <div>
-        <div className="itemLeft">
-          <img alt="" src={profesor1} className="foto-profesor"></img>
+      <div className='grid'>
+        <div className='cell'>
+          <div className="itemLeft">
+            <img alt="" src={profesor1} className="foto-profesor"></img>
+          </div>
+          <div className="itemRight">
+            <b>Profesor: </b>
+            <span className="tit">Eladio Ocaña </span> <br />
+            <b>E-mail: </b>
+            <a href="mailto:eocana@uni.edu.pe" className="tit2">
+              eocana@uni.edu.pe
+            </a>{' '}
+            <br />
+            <b>Función: </b> Coordinador
+            <br />
+            <b>Grado académico: </b> Doctor
+            <br />
+            <b>CV: </b>
+            {/* <a href="#" className="tit">CV:</a>*/}
+            <a href="#">
+              <FontAwesomeIcon className="icons2" icon={faFileAlt} />
+            </a>{' '}
+          </div>
         </div>
-        <div className="itemRight">
-          <b>Profesor: </b>
-          <span className="tit">Eladio Ocaña </span> <br />
-          <b>E-mail: </b>
-          <a href="mailto:eocana@uni.edu.pe" className="tit2">
-            eocana@uni.edu.pe
-          </a>{' '}
-          <br />
-          <b>Función: </b> Coordinador
-          <br />
-          <b>Grado académico: </b> Doctor
-          <br />
-          <b>CV: </b>
-          {/* <a href="#" className="tit">CV:</a>*/}
-          <a href="#">
-            <FontAwesomeIcon className="icons2" icon={faFileAlt} />
-          </a>{' '}
+        <div className='cell'>
+          <div className="itemLeft">
+            <img alt="" src={profesor2} className="foto-profesor"></img>
+          </div>
+          <div className="itemRight">
+            <b>Profesor: </b>
+            <span className="tit">Ernesto Oré</span> <br />
+            <b>E-mail: </b>
+            <a href="mailto:eore@uni.edu.pe" className="tit2">
+              eore@uni.edu.pe
+            </a>{' '}
+            <br />
+            <b>Función: </b> Investigador
+            <br />
+            <b>Grado académico: </b> Doctor
+            <br />
+            <b>CV: </b>
+            {/* <a href="#" className="tit">CV:</a>*/}
+            <a href="#">
+              <FontAwesomeIcon className="icons2" icon={faFileAlt} />
+            </a>{' '}
+          </div>
         </div>
-        <div className="itemLeft">
-          <img alt="" src={profesor2} className="foto-profesor"></img>
+        <div className='cell'>
+          <div className="itemLeft">
+            <img alt="" src={profesor3} className="foto-profesor"></img>
+          </div>
+          <div className="itemRight">
+            <b>Profesor: </b>
+            <span className="tit">Luis Flores </span> <br />
+            <b>E-mail: </b>
+            <a href="mailto:lfloresl@uni.edu.pe" className="tit2">
+              lfloresl@uni.edu.pe
+            </a>{' '}
+            <br />
+            <b>Función: </b> Investigador
+            <br />
+            <b>Grado académico: </b> Doctor
+            <br />
+            <b>CV: </b>
+            {/* <a href="#" className="tit">CV:</a>*/}
+            <a href="#">
+              <FontAwesomeIcon className="icons2" icon={faFileAlt} />
+            </a>{' '}
+          </div>
         </div>
-        <div className="itemRight">
-          <b>Profesor: </b>
-          <span className="tit">Ernesto Oré</span> <br />
-          <b>E-mail: </b>
-          <a href="mailto:eore@uni.edu.pe" className="tit2">
-            eore@uni.edu.pe
-          </a>{' '}
-          <br />
-          <b>Función: </b> Investigador
-          <br />
-          <b>Grado académico: </b> Doctor
-          <br />
-          <b>CV: </b>
-          {/* <a href="#" className="tit">CV:</a>*/}
-          <a href="#">
-            <FontAwesomeIcon className="icons2" icon={faFileAlt} />
-          </a>{' '}
+
+        <div className='cell'>
+          <div className="itemLeft">
+            <img alt="" src={profesor4} className="foto-profesor"></img>
+          </div>
+          <div className="itemRight">
+            <b>Profesor:</b>
+            <span className="tit">Pedro Canales </span> <br />
+            <b>E-mail: </b>
+            <a href="mailto:pcanales@uni.edu.pe" className="tit2">
+              pcanales@uni.edu.pe
+            </a>{' '}
+            <br />
+            <b>Función: </b> Investigador
+            <br />
+            <b>Grado académico: </b> Doctor
+            <br />
+            <b>CV: </b>
+            {/* <a href="#" className="tit">CV:</a>*/}
+            <a href="#">
+              <FontAwesomeIcon className="icons2" icon={faFileAlt} />
+            </a>{' '}
+          </div>
         </div>
-        <div className="itemLeft">
-          <img alt="" src={profesor3} className="foto-profesor"></img>
+
+        <div className='cell'>
+          <div className="itemLeft">
+            <img alt="" src={profesor5} className="foto-profesor"></img>
+          </div>
+          <div className="itemRight">
+            <b>Profesor: </b>
+            <span className="tit">Frank Navarro </span> <br />
+            <b>E-mail: </b>
+            <a href="mailto:fnavarro@uni.edu.pe" className="tit2">
+              fnavarro@uni.edu.pe
+            </a>{' '}
+            <br />
+            <b>Función: </b> Investigador
+            <br />
+            <b>Grado académico: </b> Doctor
+            <br />
+            <b>CV: </b>
+            {/* <a href="#" className="tit">CV:</a>*/}
+            <a href="#">
+              <FontAwesomeIcon className="icons2" icon={faFileAlt} />
+            </a>{' '}
+          </div>
         </div>
-        <div className="itemRight">
-          <b>Profesor: </b>
-          <span className="tit">Luis Flores </span> <br />
-          <b>E-mail: </b>
-          <a href="mailto:lfloresl@uni.edu.pe" className="tit2">
-            lfloresl@uni.edu.pe
-          </a>{' '}
-          <br />
-          <b>Función: </b> Investigador
-          <br />
-          <b>Grado académico: </b> Doctor
-          <br />
-          <b>CV: </b>
-          {/* <a href="#" className="tit">CV:</a>*/}
-          <a href="#">
-            <FontAwesomeIcon className="icons2" icon={faFileAlt} />
-          </a>{' '}
+
+        <div className='cell'>
+          <div className="itemLeft">
+            <img alt="" src={profesor6} className="foto-profesor"></img>
+          </div>
+          <div className="itemRight">
+            <b>Profesor: </b>
+            <span className="tit">Rosulo Perez </span> <br />
+            <b>E-mail: </b>
+            <a href="mailto:rperezc@uni.edu.pe" className="tit2">
+              rperezc@uni.edu.pe
+            </a>{' '}
+            <br />
+            <b>Función: </b> Investigador
+            <br />
+            <b>Grado académico: </b> Doctor
+            <br />
+            <b>CV: </b>
+            {/* <a href="#" className="tit">CV:</a>*/}
+            <a href="#">
+              <FontAwesomeIcon className="icons2" icon={faFileAlt} />
+            </a>{' '}
+          </div>
         </div>
-        <div className="itemLeft">
-          <img alt="" src={profesor4} className="foto-profesor"></img>
+
+        <div className='cell'>
+          <div className="itemLeft">
+            <img alt="" src={profesorX} className="foto-profesor"></img>
+          </div>
+          <div className="itemRight">
+            <b>Profesor: </b>
+            <span className="tit">Héctor Guimaray </span> <br />
+            <b>E-mail: </b>
+            <a href="mailto:hguimaray@uni.edu.pe" className="tit2">
+              hguimaray@uni.edu.pe
+            </a>{' '}
+            <br />
+            <b>Función: </b> Investigador
+            <br />
+            <b>Grado académico: </b> Doctor
+            <br />
+            <b>CV: </b>
+            {/* <a href="#" className="tit">CV:</a>*/}
+            <a href="#">
+              <FontAwesomeIcon className="icons2" icon={faFileAlt} />
+            </a>{' '}
+          </div>
         </div>
-        <div className="itemRight">
-          <b>Profesor:</b>
-          <span className="tit">Pedro Canales </span> <br />
-          <b>E-mail: </b>
-          <a href="mailto:pcanales@uni.edu.pe" className="tit2">
-            pcanales@uni.edu.pe
-          </a>{' '}
-          <br />
-          <b>Función: </b> Investigador
-          <br />
-          <b>Grado académico: </b> Doctor
-          <br />
-          <b>CV: </b>
-          {/* <a href="#" className="tit">CV:</a>*/}
-          <a href="#">
-            <FontAwesomeIcon className="icons2" icon={faFileAlt} />
-          </a>{' '}
+
+        <div className='cell'>
+          <div className="itemLeft">
+            <img alt="" src={profesorX} className="foto-profesor"></img>
+          </div>
+          <div className="itemRight">
+            <b>Profesor: </b>
+            <span className="tit">William Echegaray </span> <br />
+            <b>E-mail: </b>
+            <a href="mailto:wechegaray@uni.edu.pe" className="tit2">
+              wechegaray@uni.edu.pe
+            </a>{' '}
+            <br />
+            <b>Función: </b> Investigador
+            <br />
+            <b>Grado académico: </b> Magister
+            <br />
+            <b>CV: </b>
+            {/* <a href="#" className="tit">CV:</a>*/}
+            <a href="#">
+              <FontAwesomeIcon className="icons2" icon={faFileAlt} />
+            </a>{' '}
+          </div>
         </div>
-        <div className="itemLeft">
-          <img alt="" src={profesor5} className="foto-profesor"></img>
-        </div>
-        <div className="itemRight">
-          <b>Profesor: </b>
-          <span className="tit">Frank Navarro </span> <br />
-          <b>E-mail: </b>
-          <a href="mailto:fnavarro@uni.edu.pe" className="tit2">
-            fnavarro@uni.edu.pe
-          </a>{' '}
-          <br />
-          <b>Función: </b> Investigador
-          <br />
-          <b>Grado académico: </b> Doctor
-          <br />
-          <b>CV: </b>
-          {/* <a href="#" className="tit">CV:</a>*/}
-          <a href="#">
-            <FontAwesomeIcon className="icons2" icon={faFileAlt} />
-          </a>{' '}
-        </div>
-        <div className="itemLeft">
-          <img alt="" src={profesor6} className="foto-profesor"></img>
-        </div>
-        <div className="itemRight">
-          <b>Profesor: </b>
-          <span className="tit">Rosulo Perez </span> <br />
-          <b>E-mail: </b>
-          <a href="mailto:rperezc@uni.edu.pe" className="tit2">
-            rperezc@uni.edu.pe
-          </a>{' '}
-          <br />
-          <b>Función: </b> Investigador
-          <br />
-          <b>Grado académico: </b> Doctor
-          <br />
-          <b>CV: </b>
-          {/* <a href="#" className="tit">CV:</a>*/}
-          <a href="#">
-            <FontAwesomeIcon className="icons2" icon={faFileAlt} />
-          </a>{' '}
-        </div>
-        <div className="itemLeft">
-          <img alt="" src={profesorX} className="foto-profesor"></img>
-        </div>
-        <div className="itemRight">
-          <b>Profesor: </b>
-          <span className="tit">Héctor Guimaray </span> <br />
-          <b>E-mail: </b>
-          <a href="mailto:hguimaray@uni.edu.pe" className="tit2">
-            hguimaray@uni.edu.pe
-          </a>{' '}
-          <br />
-          <b>Función: </b> Investigador
-          <br />
-          <b>Grado académico: </b> Doctor
-          <br />
-          <b>CV: </b>
-          {/* <a href="#" className="tit">CV:</a>*/}
-          <a href="#">
-            <FontAwesomeIcon className="icons2" icon={faFileAlt} />
-          </a>{' '}
-        </div>
-        <div className="itemLeft">
-          <img alt="" src={profesorX} className="foto-profesor"></img>
-        </div>
-        <div className="itemRight">
-          <b>Profesor: </b>
-          <span className="tit">William Echegaray </span> <br />
-          <b>E-mail: </b>
-          <a href="mailto:wechegaray@uni.edu.pe" className="tit2">
-            wechegaray@uni.edu.pe
-          </a>{' '}
-          <br />
-          <b>Función: </b> Investigador
-          <br />
-          <b>Grado académico: </b> Magister
-          <br />
-          <b>CV: </b>
-          {/* <a href="#" className="tit">CV:</a>*/}
-          <a href="#">
-            <FontAwesomeIcon className="icons2" icon={faFileAlt} />
-          </a>{' '}
-        </div>
-        <div className="itemLeft">
-          <img alt="" src={profesor7} className="foto-profesor"></img>
-        </div>
-        <div className="itemRight">
-          <b>Profesor: </b>
-          <span className="tit">Jesús Cernades </span> <br />
-          <b>E-mail: </b>
-          <a href="mailto:jesus.cernades.g@uni.edu.pe" className="tit2">
-            jesus.cernades.g@uni.edu.pe
-          </a>{' '}
-          <br />
-          <b>Función: </b> Investigador
-          <br />
-          <b>Grado académico: </b> Magister
-          <br />
-          <b>CV: </b>
-          {/* <a href="#" className="tit">CV:</a>*/}
-          <a href="#">
-            <FontAwesomeIcon className="icons2" icon={faFileAlt} />
-          </a>{' '}
+
+        <div className='cell'>
+          <div className="itemLeft">
+            <img alt="" src={profesor7} className="foto-profesor"></img>
+          </div>
+          <div className="itemRight">
+            <b>Profesor: </b>
+            <span className="tit">Jesús Cernades </span> <br />
+            <b>E-mail: </b>
+            <a href="mailto:jesus.cernades.g@uni.edu.pe" className="tit2">
+              jesus.cernades.g@uni.edu.pe
+            </a>{' '}
+            <br />
+            <b>Función: </b> Investigador
+            <br />
+            <b>Grado académico: </b> Magister
+            <br />
+            <b>CV: </b>
+            {/* <a href="#" className="tit">CV:</a>*/}
+            <a href="#">
+              <FontAwesomeIcon className="icons2" icon={faFileAlt} />
+            </a>{' '}
+          </div>
         </div>
       </div>
     </>
