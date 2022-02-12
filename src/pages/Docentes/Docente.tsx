d72df2b986e332c2444c24592d59e9b0908f87b5
Carlos Felix <carloncho2357@hotmail.com>
2022-02-12 12:51:36 -0500

    Cambiado estructira de los nombres de los docentes

--- src/pages/Docentes/Docente.tsx
+++ src/pages/Docentes/Docente.tsx
@@ -5,13 +5,32 @@ function Docente(props: any) {
   const reverse = (s:any) =>{
       return s.split("").reverse().join("");
   }
+  const titleCase = (str : String) =>  {
+    var splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        // You do not need to check if i is larger than splitStr length, as your for does that for you
+        // Assign it back to the array
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+    }
+    // Directly return the joined string
+    return splitStr.join(' '); 
+  }
+
+  const obtenernombreapellido = ( nombre :any) => {
+    let nombres = nombre.split(",");
+    let primernombre = nombres[1].split(" ")[1];
+    let primerapellido = nombres[0].split(" ")[0];
+    return titleCase(primernombre +  " " + primerapellido);
+  }
+
+
   return (
     <div className="App-profes">
       <table cellPadding={8}>
         <tr>
           <td className="App-profes-nom" colSpan={2}>
             {" "}
-            <b>{props.nombre}</b>
+            <b>{obtenernombreapellido(props.nombre)}</b>
           </td>
         </tr>
         <tr>
