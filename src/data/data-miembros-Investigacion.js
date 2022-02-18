ae22dc31f8dbc2166bad583e57573c03258d0e95
abrahanrm <araymundom@uni.pe>
2022-02-18 12:35:14 -0500

    agregando data miembros

---
+++ src/data/data-miembros-Investigacion.js
@@ -0,0 +1,188 @@
+exports.miembrosOptimizacion = [
+    {
+      "nombre": "Eladio Ocaña",
+      "foto": "./eladio_ocania_carnet.jpeg",
+      "email": "eocana@uni.edu.pe",
+      "funcion": "Coordinador",
+      "gradoAc": "Doctor",
+      "cv": "https://ctivitae.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=213",
+    },
+    {
+      "nombre": "Ernesto Oré",
+      "foto": "ernesto_ore_carnet.png",
+      "email": "eore@uni.edu.pe",
+      "funcion": "Investigador",
+      "gradoAc": "Doctor",
+      "cv": "http://dina.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=39687",
+    },
+    {
+      "nombre": "Luis Flores",
+      "foto": "luis_flores_carnet.png",
+      "email": "lfloresl@uni.edu.pe",
+      "funcion": "Investigador",
+      "gradoAc": "Doctor",
+      "cv": "https://ctivitae.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=90685",
+    },
+    {
+      "nombre": "Pedro Canales",
+      "foto": "./pedro_canales_carnet.png",
+      "email": "pcanales@uni.edu.pe",
+      "funcion": "Investigador",
+      "gradoAc": "Doctor",
+      "cv": "https://ctivitae.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=28421",
+    },
+    {
+      "nombre": "Leopoldo Paredes",
+      "foto": "./docentes/leopoldo_paredes_carnet.png",
+      "email": "lparedess@uni.edu.pe",
+      "funcion": "Investigador",
+      "gradoAc": "Licenciado",
+      "cv": "https://ctivitae.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=95237",
+    },
+    {
+      "nombre": "Johnny Valverde",
+      "foto": "johnny_valverde_carnet.PNG",
+      "email": "jvalverde@uni.edu.pe",
+      "funcion": "Investigador",
+      "gradoAc": "Magister ",
+      "cv": "http://dina.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do;jsessionid=c98aec269452ababfabdf9a9343c?id_investigador=22865",
+    },
+    {
+      "nombre": "Frank Navarro",
+      "foto": "frank_navarro_carnet.png",
+      "email": "fnavarro@uni.edu.pe",
+      "funcion": "Investigador",
+      "gradoAc": "Doctor",
+      "cv": "http://dina.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=16961",
+    },
+    {
+      "nombre": "Rosulo Perez",
+      "foto": "./rosulo_perez_carnet.png",
+      "email": "rperezc@uni.edu.pe",
+      "funcion": "Investigador",
+      "gradoAc": "Doctor",
+      "cv": "https://ctivitae.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=47587",
+    },
+    {
+      "nombre": "Hector Guimaray",
+      "foto": "./fotoblanco.jpg",
+      "email": "hguimaray@uni.edu.pe",
+      "funcion": "Investigador",
+      "gradoAc": "Doctor",
+      "cv": "http://dina.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do;jsessionid=c7d8dca9f9ca5d4dc24752a57f87?id_investigador=57967",
+    },
+    {
+      "nombre": "Jesús Cernades",
+      "foto": "./jesus_cernades_carnet.png",
+      "email": "jesus.cernades.g@uni.edu.pe",
+      "funcion": "Investigador",
+      "gradoAc": "Magister",
+      "cv": "http://dina.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=36929",
+    }
+  ];
+
+exports.miembrosTeoriaDeNumeros = [
+    {
+      "nombre": "Oswaldo Velasquez",
+      "foto": "oswaldo_velasquez_carnet.png",
+      "email": "ovelasquez@uni.edu.pe",
+      "funcion": "Coordinador",
+      "gradoAc": "Doctor",
+      "cv": "https://ctivitae.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=16951",
+      }
+  ];
+
+  exports.miembrosAlgebra = [
+    {
+      "nombre": "Joe Palacios",
+      "foto": "joe_palacios_carnet.png",
+      "email": "jpalaciosb@uni.edu.pe",
+      "funcion": "Coordinador",
+      "gradoAc": "Doctor",
+      "cv": "http://dina.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=45263",
+      }
+  ];
+
+  exports.miembrosProbabilidad = [
+    {
+      "nombre": "Gonzalo Panizo",
+      "foto": "gonzalo_panizo_carnet.png",
+      "email": "gpanizog@uni.edu.pe",
+      "funcion": "Coordinador",
+      "gradoAc": "Doctor",
+      "cv": "http://dina.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=14546",
+    }
+  ];
+
+  exports.miembrosEcuacionesDiferenciales = [
+    {
+      "nombre": "Roger Metzger",
+      "foto": "roger_metzger_carnet.png",
+      "email": "rmetzger@uni.edu.pe",
+      "funcion": "Coordinador",
+      "gradoAc": "Doctor",
+      "cv": "https://ctivitae.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=2056",
+    }
+  ];
+
+
+  exports.miembroAnalisisNumerico = [
+    {
+      "nombre": "Ángel Ramirez",
+      "foto": "./angel_ramirez_carnet.png",
+      "email": "aramirezg@uni.edu.pe",
+      "funcion": "Coordinador",
+      "gradoAc": "Doctor",
+      "cv": "http://dina.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=27285",
+    },
+    {
+      "nombre": "Irla Mantilla",
+      "foto": "./irla_mantilla_carnet.png",
+      "email": "irlamn@uni.edu.pe",
+      "funcion": "Investigadora",
+      "gradoAc": "Doctora",
+      "cv": "https://ctivitae.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=15560",
+    },
+    {
+      "nombre": "Jonathan Munguia",
+      "foto": "./fotomunguia.jpeg",
+      "email": "jmunguial@uni.edu.pe",
+      "funcion": "Investigador",
+      "gradoAc": "Doctor",
+      "cv": "http://dina.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=82909",
+    },
+    {
+      "nombre": "Luis Roca",
+      "foto": "luis_roca_carnet.png",
+      "email": "lrocag@uni.edu.pe",
+      "funcion": "Investigador",
+      "gradoAc": "Licenciado",
+      "cv": "https://ctivitae.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=401"
+    },
+    {
+      "nombre": "Josue Zacarías Olivet Rojas",
+      "foto": "./fotoblanco.jpg",
+      "email": "correo@uni.edu.pe",
+      "funcion": "Investigador",
+      "gradoAc": "Bachiller",
+      "cv": "" 
+    },
+    {
+      "nombre": "Kriss Lady Gutierrez Anco",
+      "foto": "./fotoblanco.jpg",
+      "email": "correo@uni.edu.pe",
+      "funcion": "Investigadora",
+      "gradoAc": "Bachiller",
+      "cv": ""
+    },
+    {
+      "nombre": "Marcia P.Nesiosup Vilca",
+      "foto": "./fotoblanco.jpg",
+      "email": "correo@uni.edu.pe",
+      "funcion": "Investigadora",
+      "gradoAc": "Bachiller",
+      "cv": ""
+    }
+  ];
+
+
