f07ec7c027cb68eabf0612bdd6524c35beab96d2
Carlos Felix <carloncho2357@hotmail.com>
2022-02-17 23:36:15 -0500

    se quitó a Heiz de testimonios, se cambió la descripción del videos de janice

--- src/data/data-testimonios.js
+++ src/data/data-testimonios.js
@@ -6,12 +6,6 @@ exports.testimonios = [
     "testimony": "Matemático de formación y autodidacta en temas computacionales. Egresado de la Facultad de Ciencias de la Universidad Nacional de Ingeniería. Con experiencia en centros de investigación como el Radio Observatorio de Jicamarca desarrollando modelos para analizar datos de alta atmósfera usando análisis de datos, machine learning y computación paralela. Además, laboré en el Laboratorio Costero de Paita (IMARPE) estudiando modelos que mejoran los aspectos predictivos de la pesca artesanal. También en el Centro de Investigación de la Universidad del Pacífico. Últimamente he laborado en empresas del mercado de servicios TI, que se encargan de dar servicios de desarrollo de software y ‘’business intelligence’’ a empresas de diversos rubros del mercado nacional e internacional. De manera paralela a las actividades de investigación y desarrollo me desempeño dictando cursos en diplomados sobre estadística aplicada, business intelligente, machine learning y deep learning en instituciones académicas públicas y privadas.",
   },
   {
-    "name": "Heinz Guillermo Roque Loyola",
-    "image": "./Heinz_perfil.jpeg",
-    "fotoCompleta": "./Heinz_completa.jpeg",
-    "testimony": "Egresado y Licenciado de la Escuela de Matemática de la Facultad de Ciencias de 1996 al 2002, obtuvo una maestría en Estadística en la Pontificia Universidad Católica de Chile de 2010 al 2012, obtuvo un máster en ciencias y finanzas en el IEDE Business School con sede en Chile de 2013 al 2014 y recibió un grado en ‘’Alianzas Público-Privadas: Implementando Soluciones para América Latina y el Caribe’’ en el 2015. Se desempeñó como Consultor Senior en Asociaciones Público-Privadas en IKONS ATN, donde se desempeñó como Modelador Financiero, Analista de Riesgos, Analista Cuantitativo y Cualitativo de Información y Desarrollador de aplicaciones y/o complementos para la Toma de Decisiones. Ha sido un gran amigo e impulsor de las matemáticas en el sector financiero, con mucha tristeza falleció en Lima, el 26 de abril de 2021.",
-  },
-  {
     "name": "Daniel Camarena Perez",
     "image": "./daniel_camarena_carnet.png",
     "fotoCompleta": "./daniel_camarena_completa.jpeg",
