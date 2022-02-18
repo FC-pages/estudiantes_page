37de55c2637f19b1b378d1ade18453ed3b683b0f
Janice Escobedo <janice.ev224@gmail.com>
2022-02-17 21:18:20 -0500

    mensaje

--- src/pages/Descripcion/TabsContent/ResenaHistorica.tsx
+++ src/pages/Descripcion/TabsContent/ResenaHistorica.tsx
@@ -5,17 +5,16 @@ function ResenaHistorica() {
   return (
       
           <div className="App-cuerpo">
-            <article id="tab2">
-              <table className="App-resena" cellSpacing={0} cellPadding={5}>
+              <table className="resena" cellSpacing={0} cellPadding={5}>
                 <tr>
                   <td>
                     <b className="anios"> 1960</b>
-                    <p className="App-resena-borde">
+                    <p className="resena-borde">
                       {" "}
-                      Se creó la Facultad de Ciencias Físicas y Matemáticas en
-                      la Universidad Nacional de Ingeniería con las carreras de
-                      Física Química y Matemática, a partir de la entonces
-                      Facultad de Ciencias Básicas y Nucleares.
+                      Promulgada la Ley Universitaria No. 13417, se creó la Facultad de 
+                      Ciencias Físicas y Matemáticas en la Universidad Nacional de 
+                      Ingeniería, a partir de la entonces Facultad de Ciencias Básicas 
+                      y Nucleares.
                     </p>
                   </td>
                   <td> </td>
@@ -24,102 +23,83 @@ function ResenaHistorica() {
                   <td></td>
                   <td>
                     <b className="anios">1961</b>
-                    <p className="App-resena-borde">
-                      Se aprobó el Plan de Estudios, que consideró cursos
-                      semestralizados en la Facultad a partir del segundo año,
-                      pues los estudiantes ingresaban a la Universidad a través
-                      del Departamento Preparatorio, realizando un primer año
-                      (dos semestres) común a todas las Facultades. Se aprobó
-                      también otorgar el Grado de Bachiller en Ciencias Físicas
-                      y Matemáticas después de concluidos el cuarto año y previa
-                      sustentación de una tesis.
+                    <p className="resena-borde">
+                    Se aprueba el Plan de Estudios, que considera cursos semestralizados en 
+                    la Facultad a partir del segundo año, pues en ese entonces los 
+                    estudiantes ingresaban a la Universidad a través del Departamento 
+                    Preparatorio, realizando un primer año (dos semestres) común a todas 
+                    las Facultades. Se aprueba también otorgar el Grado de Bachiller en 
+                    Ciencias Físicas y Matemáticas después de concluir el cuarto año y 
+                    previa sustentación de una tesis.
                     </p>
                   </td>
                 </tr>
                 <tr>
+                  <td>
+                    <b className="anios">1962</b>
+                    <p className="resena-borde">
+                      A iniciativa del Dr. José Tola Pasquel, se crea el Instituto de 
+                      Matemática de la UNI (IMUNI), siendo el Ing. Mario Samamé Boggio, rector 
+                      de la UNI. Lamentablemente por cuestiones políticas internas (en la UNI) 
+                      y externas (en el gobierno) hicieron que IMUNI cerrara en 1968.</p>
+                  </td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td></td>
                   <td>
                     <b className="anios">1965</b>
-                    <p className="App-resena-borde">
-                      Se modifica el Plan de Estudios, se considera el primer
-                      año común de las Facultades, mantiene los tres años
-                      siguientes, al final de los cuales se opta el Grado de
-                      Bachiller en Ciencias Físicas y Matemáticas, previa
-                      sustentación de una tesis; y crea un Quinto Año en la
-                      Facultad en el que el estudiante podrá especializarse en
-                      Física o Matemática y optar el Título Profesional de
-                      Físico-Matemático, previa sustentación de una tesis.
+                    <p className="resena-borde">
+                      Se modifica el Plan de Estudios, se considera el primer año común de las 
+                      Facultades con énfasis en los cursos de álgebra, análisis y física 
+                      general, manteniéndose los tres años siguientes, al final de los cuales 
+                      se opta el Grado de Bachiller en Ciencias Físicas y Matemáticas previa 
+                      sustentación de una tesis; y se crea también el Quinto Año en la Facultad
+                      en el que el estudiante pueda especializarse en Física o Matemática y 
+                      optar el Título Profesional de Físico-Matemático, previa sustentación de 
+                      una tesis.
                     </p>
                   </td>
-                  <td></td>
                 </tr>
                 <tr>
-                  <td></td>
                   <td>
                     <b className="anios">1967</b>
-                    <p className="App-resena-borde">
-                      Se modifica nuevamente el Plan de Estudios, se incrementa
-                      los cursos de Química a 3, y se establece que el quinto
-                      año (etapa de especialización) tiene una orientación
-                      profesional y otra académica. La profesión conduce al
-                      Título de Físico-Matemático e involucra una preparación en
-                      Ciencia Aplicada, mientras que la académica corresponde a
-                      una preparación adecuada para continuar estudios en
+                    <p className="resena-borde">
+                      Se modifica nuevamente el Plan de Estudios, se incrementa a tres el 
+                      número de cursos de Química, y se establece que el quinto año (etapa de 
+                      especialización) tenga dos orientaciones, profesional y académica. La 
+                      orientación profesional conduce al Título de Físico-Matemático e 
+                      involucra una preparación en Ciencia Aplicada; la orientación académica 
+                      corresponde a una preparación adecuada para continuar estudios en 
                       Postgrado.
                     </p>
                   </td>
-                </tr>
-                <tr>
-                  <td>
-                    <b className="anios">1970</b>
-                    <p className="App-resena-borde">
-                      Se aprueba otorgar los Grados de Bachiller en Ciencias con
-                      mención en Física, en Matemática, en Química y en
-                      Estadística, así como los Títulos Profesionales de
-                      Licenciado en cada una de las mencionadas especialidades.
-                      El Grado y el Título se optan después de cumplir los 10
-                      semestres académicos que considera el Plan de Estudios,
-                      previa sustentación de las respectivas tesis. Cabe
-                      mencionar que los estudios en Estadística no se iniciaron
-                      sino en el año 1977.
-                    </p>
-                  </td>
                   <td> </td>
                 </tr>
                 <tr>
                   <td> </td>
                   <td>
-                    <b className="anios">1983</b>
-                    <p className="App-resena-borde">
-                      Se promulga la Ley Universitaria No.23733 que modifica
-                      nuevamente la estructura académica y administrativa de las
-                      universidades y se crean las actuales Facultades, entre
-                      ellas la Facultad de Ciencias (FC), estableciéndose las
-                      Escuelas Profesionales de Física, Matemática, Química y
-                      Estadística. Se otorgan los Grados de Bachiller en
-                      Ciencias y los Títulos Profesionales de Licenciado,
-                      respectivos.
+                    <b className="anios">1969</b>
+                    <p className="resena-borde">
+                    Se decreta la Ley Universitaria No.17437, cambiando la estructura 
+                    académica y administrativa de las universidades. Se elimina el régimen 
+                    facultativo y se crea el de Departamentos y Programas Académicos. Es así 
+                    como se crea el Programa Académico de Ciencias (PAC) con las 
+                    especialidades de Física, Matemática y Química.
                     </p>
                   </td>
                 </tr>
                 <tr>
                   <td>
                     <img alt="" src="./impa.png" />
-                    <b className="anios"> 1987</b>
-                    <p className="App-resena-borde">
-                      Se creó la Escuela de Ingeniería Física y la Escuela
-                      Profesional de Ciencia de la Computación, por lo que
-                      actualmente la Facultad de Ciencias cuenta con las
-                      especialidades de FÍSICA, MATEMÁTICA, QUÍMICA, INGENIERIA
-                      FÍSICA y CIENCIA DE LA COMPUTACIÓN. A iniciativa de la
-                      Facultad de Ciencias se creó, el INSTITUTO DE MATEMÁTICA Y
-                      CIENCIAS AFINES (IMCA), cuyos objetivos son la
-                      investigación y el apoyo a la formación de Maestros y
-                      Doctores, en Matemática; para lo cual se cuenta con la
-                      colaboración del Instituto de Matemática Pura y Aplicada
-                      (IMPA) de Río de Janeiro, Brasil. El IMCA funciona
-                      actualmente en una moderna instalación en el distrito
-                      limeño de La Molina construido por el Patronato UNI que
-                      preside el Ing. Alberto Benavides de la Quintana.{" "}
+                    <b className="anios"> 1983</b>
+                    <p className="resena-borde">
+                      Se promulga la Ley Universitaria No.23733 que modifica nuevamente la 
+                      estructura académica y administrativa de las universidades y se crean 
+                      las actuales Facultades, entre ellas la Facultad de Ciencias (FC), 
+                      estableciéndose las Escuelas Profesionales de Física, Matemática, 
+                      Química y Estadística. Se otorgan los Grados de Bachiller en Ciencias 
+                      y los respectivos Títulos Profesionales de Licenciado.{" "}
                     </p>
                   </td>
                   <td> </td>
@@ -127,27 +107,52 @@ function ResenaHistorica() {
                 <tr>
                   <td> </td>
                   <td>
-                    <b className="anios">2010</b>
-                    <p className="App-resena-borde">
-                      La Facultad de Ciencias ha cumplido 50 años de su creación
-                      con actividades sociales, culturales, científicas y
-                      entrega de títulos honoríficos a egresados, publicándose
-                      el libro "50 años de la Facultad de Ciencias", así mismo
-                      se ha continuado con cierta intensidad, contando con el
-                      apoyo del Rectorado de la Universidad, el mejoramiento de
-                      los equipos de nuestros laboratorios de enseñanza,
-                      investigación y de servicios iniciándose asimismo el apoyo
-                      financiero de las empresas mineras (Southern Peru Copper
-                      Corporation) para estos propósitos. Se nos ha entregado
-                      por Recursos Ordinarios S/. 1 200 000 para equipamiento de
-                      un Laboratorio de Química y S/. 970 371,00 para iniciar la
-                      construcción de nuevos pabellones, para el Auditorio,
-                      Biblioteca, Sala de Lectura, Laboratorios, entre otros.
+                    <b className="anios">1997</b>
+                    <p className="resena-borde">
+                    A iniciativa de la Facultad de Ciencias se creó, mediante Resolución 
+                    Rectoral No. 000109 del 14.2.97, el Instituto de Matemática y Ciencias 
+                    Afines (IMCA), cuyos objetivos son la investigación y el apoyo a la 
+                    formación de Maestros y Doctores en Matemática. Desde el 2006 el IMCA 
+                    viene funcionando en una moderna instalación en el distrito limeño de 
+                    La Molina construido por el Patronato UNI precidido en ese entonces por 
+                    el Ing. Alberto Benavides de la Quintana. 
                     </p>
                   </td>
                 </tr>
+                <tr>
+                  <td>
+                    <img alt="" src="./impa.png" />
+                    <b className="anios"> 2010</b>
+                    <p className="resena-borde">
+                      La Facultad de Ciencias cumple 50 años de su creación con actividades 
+                      sociales, culturales, científicas y entrega de títulos honoríficos a 
+                      egresados, publicándose el libro "50 años de la Facultad de Ciencias".{" "}
+                    </p>
+                  </td>
+                  <td> </td>
+                </tr>
+                <tr>
+                  <td> </td>
+                  <td>
+                    <b className="anios">2012</b>
+                    <p className="resena-borde">
+                      Resolución Rectoral Nº 1481 del 3.11.09 se creó la Escuela Profesional 
+                      de Ciencia de la Computación.
+                    </p>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <img alt="" src="./impa.png" />
+                    <b className="anios"> act.</b>
+                    <p className="resena-borde">
+                      La Facultad de Ciencias cuenta con las especialidades de Física, 
+                      Matemática, Química, Ingeniería física y Ciencias de la computación. {" "}
+                    </p>
+                  </td>
+                  <td> </td>
+                </tr>
               </table>
-            </article>
           </div>
 
 
