6a0f9846bf07fa5aeccc3593a1c011b96e3bbd26
Abrahan R. Muñoa <45294233+abrahanrm@users.noreply.github.com>
2022-03-19 12:21:14 -0500

    add publicacion en EDySD

--- src/pages/LineasInvestigacion/EDySD/Publicaciones.tsx
+++ src/pages/LineasInvestigacion/EDySD/Publicaciones.tsx
@@ -1,6 +1,8 @@
 import React, { FC, Fragment } from "react";
 /*import table from "../table.png";*/
 import "../../../styles/gInvestigacion.css";
+import { EDySD_publicaciones } from "../../../data/publicaciones.js";
+import Publicacion from '../Publicacion';
 
 const TabFour: FC<{}> = () => {
   return (
@@ -8,6 +10,16 @@ const TabFour: FC<{}> = () => {
       <div>
         <h3 className="t-stroke-shadow">Línea de Ecuaciones Diferenciales y Sistemas Dinámicos</h3>
       </div>
+      <hr></hr>
+
+      {
+        EDySD_publicaciones.map((publicacion) => {
+          return <Publicacion
+            pub = {publicacion}
+            />
+        })
+      }
+      <br />
       <hr></hr>
     </Fragment>
   );
