18022084c82022ef6bc92817db1f78441cddeb93
Carlos Felix <carloncho2357@hotmail.com>
2022-06-06 04:12:29 -0500

    CAmbios

--- src/pages/LineasInvestigacion/AlgebraGyTopo/Proyectos.tsx
+++ src/pages/LineasInvestigacion/AlgebraGyTopo/Proyectos.tsx
@@ -1,13 +1,57 @@
 import React, { FC, Fragment } from "react";
 import "../../../styles/gInvestigacion.css";
 
+let proyectos = [
+  {
+    titulo: "Ciclos Algebraicos sobre Superficies Algebraicas",
+    i : "FC-UNI, 2017.",
+    resumen: "",
+  },
+]
+
+const Proyecto = (props: any) => {
+  return (
+    <>
+      <h4 className="tit">{ props.proy.titulo }. <i>{ props.proy.i }</i></h4>
+      <p>{ props.proy.resumen }</p>
+      <hr className="dotted"></hr>
+    </>
+  )
+}
+
 const TabThree: FC<{}> = () => {
   return (
     <Fragment>
       <div>
-        <h3 className="t-stroke-shadow">Línea de Álgebra Geometría y Topología</h3>
+        <h3 className="t-stroke-shadow">Línea de Optimización</h3>
       </div>
       <hr></hr>
+      {/* <h3>Proyectos</h3> */}
+      {/*
+      <h4 className="tit">A Inertial-Relaxed splitting algorithm for inclusion problems mixing a composite monotone plus a co-coercive operator <i>IdI, FC-UNI, 2021</i></h4>
+      <p>The increasing popularity of cluster-randomized trials has created many opportunities and challenges for statisticians. In recent years a variation of the traditional cluster randomized trial has been proposed called stepped...</p>
+      <hr className="dotted"></hr>
+      <h4 className="tit">Optimización combinatoria en grafos con incertidumbre de ubicación.  <i>Programa Ecosnord, 2020-2022</i></h4>
+      <p>Una limitación de los modelos clásicos de regresión es que estos asumen un conjunto de supuestos acerca de la naturaleza de las variables consideradas. Supuestos usuales son asumir normalidad en los errores,...</p>
+      <hr className="dotted"></hr>
+      <h4 className="tit">Stochastic Optimization and Chance Constraints with Applications to Energy. <i>Programa MathAmsud, 2020-2021</i></h4>
+      <p className="justify">La estimación de la prevalencia es una tarea sencilla si se cuentan con pruebas diagnósticas 100% exactas. Sin embargo, en muchos casos lo que ocurre es que no existen pruebas...</p>
+      <hr className="dotted"></hr>
+      <h4 className="tit">Strong duality for quadratic optimization problems. <i>IdI, FC-UNI, 2020</i></h4>
+      <p>Relación entre el desempeño en las evaluaciones para la admisión y el rendimiento en los dos primeros ciclos en la PUCP. El objetivo es analizar en qué medida las evaluaciones...</p>
+      <hr className="dotted"></hr>
+      <h4 className="tit">Algoritmos de descomposición aplicados a problemas de programación mixta. <i>IdI, FC-UNI, 2020</i></h4>
+      <p>Una limitación de los modelos clásicos de regresión es que estos asumen un conjunto de supuestos acerca de la naturaleza de las variables consideradas. Supuestos usuales son asumir normalidad en los errores,...</p>
+      <hr className="dotted"></hr>
+      */}
+      { proyectos.map((proyecto) => {
+        return (
+          <Proyecto
+            proy = {proyecto}
+          />
+        )
+      }) }
+     
     </Fragment>
   );
 };
--- src/pages/LineasInvestigacion/ProbabilidadyPE/EventosAcademicos.tsx
+++ src/pages/LineasInvestigacion/ProbabilidadyPE/EventosAcademicos.tsx
@@ -1,14 +1,40 @@
-import React, { FC, Fragment } from "react";
+import React, { FC, Fragment, useState } from "react";
+import { Modal, Button} from "react-bootstrap";
 /*import "../App.css";*/
 import "../../../styles/gInvestigacion.css";
+import * as S from "../styles/stylesLineas"
+import miembros_optimizacion from "../images/Optimizacion/foto_miembros.png";
+
 
 const TabFour: FC<{}> = () => {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <Fragment>
       <div>
-        <h3 className="t-stroke-shadow">Línea de Probabilidad y Procesos Estocásticos</h3>
+        <h3 className="t-stroke-shadow">Línea de Optimización</h3>
       </div>
       <hr></hr>
+
+      <h4 className="tit">Random Media and Random Structures in Lima</h4>
+      <div className="grid">
+        <div className="itemLeft3">
+El tema de los medios aleatorios ha tenido un rápido desarrollo durante al menos los últimos treinta años. La motivación original para los modelos en medios aleatorios provino de campos como la física de la materia condensada, la química física, la geología y otros. Hoy en día se puede decir que los medios aleatorios han impregnado casi todos los campos de la teoría de la probabilidad y los modelos aleatorios de diferente tipo.
+          <br/>
+          <br/>
+El presente congreso tiene como objetivo presentar algunos de los últimos desarrollos en el campo de Random Media y su papel en la aparición de estructuras aleatorias. 
+          <br/>
+          <br/>
+          
+    <a href="https://rmrslima2020.netlify.app/" className="tit2">https://rmrslima2020.netlify.app/</a>
+  </div>
+              </div>
+      <br/>
+      <hr className="dotted"></hr>
     </Fragment>
   );
 };
