09d7bcbc20b9715d6cd30638573d98ad7aa9c50a
Jonathan <jmunguial@uni.edu.pe>
2022-07-19 15:21:20 -0500

    teoria de numeros2

--- src/pages/LineasInvestigacion/AnalisisyTeoriadeNumeros/Acerca.tsx
+++ src/pages/LineasInvestigacion/AnalisisyTeoriadeNumeros/Acerca.tsx
@@ -17,6 +17,12 @@ const Acerca: FC<{}> = () => {
       <br />      
       <p className="justify"></p>
       En la línea de Análisis y Teoría de Números de la Facultad de Ciencias estudiamos problemas ligados al análisis de funciones reales y complejas de una y varias variables,
+      <br/> <br/>
+      y sus aplicaciones al estudio de la distribución de conjuntos de números enteros. Esto corresponde a áreas de variable compleja y teoría analítica de números. Entre otros
+      <br/> <br/>
+      temas, en lo concerniente a análisis, estudiamos problemas de distribución de ceros, polos y en general de puntos singulares de funciones meromorfas, además de
+      <br/> <br/>
+      problemas de continuación analítica de series, mientras que en lo concerniente a la teoría de números, estudiamos teoremas tauberianos y sus aplicaciones.
     </>
   );
 };
