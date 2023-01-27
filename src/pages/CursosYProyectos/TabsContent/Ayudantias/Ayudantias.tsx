5e4ad0297777edbfd8aa9405934517169e7c86ef
Jhonnatan E. Rojas <hytzy18@gmail.com>
2023-01-27 12:37:33 -0500

    se transalo ayudantias a la page correspondiente

---
+++ src/pages/CursosYProyectos/TabsContent/Ayudantias/Ayudantias.tsx
@@ -0,0 +1,54 @@
+import '../../Descripcion/styles.css';
+import * as S from "../../../AlumnosYEgresados/TabsContent/AlumnosYTesistasTab/styles/main";
+import AyudantiasCard from "../Cards/AyudantiasCard";
+import { Container,Row } from 'react-bootstrap';
+import Dropdown from 'react-bootstrap/Dropdown';
+let { data } = require("../../../data/data-ayudantias.js");
+
+function Ayudantias() {
+  return (
+      <Container>
+      <div className="mb-2">
+        <a target = "_blank" href = "https://forms.gle/kz7isKyEyMkfB3nk6" type="button" className="btn btn-success btn-lg insc">Pre-inscripción</a>
+        <a target = "_blank" href = "https://forms.gle/QqWY77726kZMkm91A" type="button" className="btn btn-warning btn-lg cancelar-insc">Cancelar ayudantía</a>
+      </div>
+      <Row>
+      <Dropdown className="d-inline">
+          <div className="d-flex align-items-center">
+          <Dropdown.Toggle id="dropdown-autoclose-true">
+              2023-1
+          </Dropdown.Toggle>
+          </div>
+          <Dropdown.Menu>
+              <Dropdown.Item>2023-1</Dropdown.Item>
+          </Dropdown.Menu>
+      </Dropdown>
+      </Row>
+      <Row>
+      <S.TabContent>
+        <S.TestimoniosTab >    
+          {data.map((t: any) => {
+            return (
+              <AyudantiasCard
+                marca={t.marca}
+                correo={t.correo}
+                nombres = {t.nombres}
+                apellidos = {t.apellidos}
+                codigo_alumno = {t.codigo_alumno}
+                celular={t.celular}                
+                curso={t.curso}
+                codigo_curso={t.codigo_curso}
+                seccion={t.seccion}
+                profesor_teoria={t.profesor_teoria}
+                profesor_practica = {t.profesor_practica}
+              />
+            );
+          })}
+        </S.TestimoniosTab>
+      </S.TabContent>  
+      </Row>        
+      </Container>
+  );
+}
+
+export default Ayudantias;
---
+++ src/pages/CursosYProyectos/TabsContent/Cards/AyudantiasCard.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { Card } from "react-bootstrap";
+
+import * as S from "../../../AlumnosYEgresados/TabsContent/AlumnosYTesistasTab/styles/main";
+
+function AyudantiasCard(e:any) {
+
+  return (
+    <S.TCard3>
+      <Card >
+        <Card.Body className="card-testimony">
+          <div className="card-testimony-header">
+            <Card.Title className="card-title">{e.curso} - {e.codigo_curso} {e.seccion}</Card.Title>
+          </div>
+          <Card.Text className="card-text">
+          Docente: {e.profesor_practica}<br></br>
+          Alumno ayudante: {e.nombres} {e.apellidos}<br></br>
+          Código alumno: {e.codigo_alumno}<br></br>
+          </Card.Text>
+        </Card.Body>
+        
+      </Card>
+    </S.TCard3>
+  );
+}
+
+export default AyudantiasCard;
