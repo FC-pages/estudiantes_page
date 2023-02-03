3a340b45d4d979280331c0842f7a6d423091a547
zzVitezz <viteomar537.b@gmail.com>
2023-02-03 13:18:17 -0500

    linksgs

--- src/helpers/linksgs.tsx
+++ src/helpers/linksgs.tsx
@@ -1,7 +1,7 @@
 // Archivo temporal
 
 const linksgs = {
-    cursos_electivos : 'https://script.google.com/macros/s/.../exec',
+    cursos_electivos : 'https://script.google.com/macros/s/AKfycbxaP58sgFCL5qTufF36Yr-PXdavwJHAGJuYgKI31jgnLt0FBBVJQyQRY2tfldd5DOQgBA/exec',
     proyecto_tesis : 'https://script.google.com/macros/s/AKfycbwSr7qcPxFimQgGuTpZP9YJlDjtTSqcyJ3qopI6luIaK_jnZXZJxgBpl06DwbkfM1M7PA/exec',
     proyecto_tesis_mungia: 'https://script.google.com/macros/s/AKfycbylw0hURDVlrqVtqXvoewqoSPbMSW9yQUtX9grtRcTCYPeRfWn9hXYSZCCiuFvERCto/exec',
     ayudantias : 'https://script.google.com/macros/s/AKfycbx6zx8TWvXC4-XGW2SNQ82KaBN16iSLDTiBwYpCKDmzbcuUSDIRW1gHbX6iLeFD9mSEog/exec',
