654692f85fadf04fd8e8e282eb2f033d7e8f0320
Jhonnatan E. Rojas <hytzy18@gmail.com>
2023-02-04 09:05:42 -0500

    se corrigio data

--- src/data/data-practicas.js
+++ src/data/data-practicas.js
@@ -1,4 +1,4 @@
-exports.data= 
+exports.data2= 
 [
   /*{
     marca: "2022-07-22T19:09:43.284Z",
@@ -10,7 +10,7 @@ exports.data=
   {"marca":"2022-09-14T00:36:33.468Z","correo":"renee.lopez.p@uni.pe","nombres":"Renee Jair","apellidos":"Lopez Punin","codigo_alumno":"20160255A","celular":972352332,"supervisor":"Jonathan Alfredo Munguia La Cotera","actividad":"Practicante tutor de matemáticas","institucion":"Universidad Tecnológica del Perú"}
 ]
 
-exports.data2= 
+exports.datatest= 
 [
   /*{
     marca: "2022-07-22T19:09:43.284Z",
