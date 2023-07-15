3beefa1d99ce1229dbdf31d5cb3a37f9d7be37df
llVitell <omar.vite.a@uni.pe>
2023-07-14 22:26:42 -0500

    Cursos Electivos y Proyecto de tesis actualizado

--- src/helpers/linksgs.tsx
+++ src/helpers/linksgs.tsx
@@ -1,8 +1,8 @@
 // Archivo temporal
 
 const linksgs = {
-    cursos_electivos : 'https://script.google.com/macros/s/AKfycbz9CLn38-gAKTxa8Rnz71t5y4Dlfh_5bJjlsapOITeSQt7_OFFPSfUcU5VGErC2kzZI/exec',
-    proyecto_tesis : 'https://script.google.com/macros/s/AKfycbzvxz57vaUBLgM5Jz55LTkMQh5H3nKwPXI6U4E-SZ7JxxNJHisRyCZQeggWHvsCYy9q/exec',
+    cursos_electivos : 'https://script.google.com/macros/s/AKfycbz-OLulkFgbn65Jy1_CjHx0MMrxVwEt69muRuubrzYVQgqNUw6XDX4L4QhZo7dSvfc0WQ/exec',
+    proyecto_tesis : 'https://script.google.com/macros/s/AKfycbzpgwVbC8LwKVXpenpPX3nCj8unEnMKB0fWgat4sWYC7TkQZbv0ua82Ir-_L3YgbeCs/exec',
     proyecto_tesis_mungia: 'https://script.google.com/macros/s/AKfycbwSr7qcPxFimQgGuTpZP9YJlDjtTSqcyJ3qopI6luIaK_jnZXZJxgBpl06DwbkfM1M7PA/exec',
     ayudantias : 'https://script.google.com/macros/s/AKfycbx6zx8TWvXC4-XGW2SNQ82KaBN16iSLDTiBwYpCKDmzbcuUSDIRW1gHbX6iLeFD9mSEog/exec',
     practicas_preprofesionales : 'https://script.google.com/macros/s/AKfycbyrmg693u0X4dfzdEf5uV1nxx0HT-esftLk2zTS0UqBXEC2MyIKiShAXWgAC4qQxn5o/exec',
